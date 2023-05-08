78e0b11c8889e132977b7d6b2966e580725f699b
Thomas Deinhamer <code@thasmo.dev>
2023-05-08 23:24:19 +0200

    Add extension configuration file

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Media Processing',
+	'description' => 'Integrates various image processing libraries and SaaS cloud services into TYPO3 by leveraging their APIs to process images.',
+	'category' => 'services',
+	'author' => 'Thomas Deinhamer',
+	'author_email' => 'code@thasmo.dev',
+	'author_company' => 'somehow.digital',
+	'state' => 'alpha',
+	'version' => '0.0.0',
+	'autoload' => [
+		'psr-4' => [
+			'SomehowDigital\\Typo3\MediaProcessing\\' => 'Classes/',
+		],
+	],
+	'constraints' => [
+		'depends' => [
+			'php' => '8.1.0-8.2.99',
+			'typo3' => '12.0.0-12.4.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
