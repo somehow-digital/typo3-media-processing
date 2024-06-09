aa80bfc795c5760536f156565918c70c14732826
Thomas Deinhamer <code@thasmo.dev>
2024-06-09 16:42:41 +0200

    Update supported PHP versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
 	],
 	'constraints' => [
 		'depends' => [
-			'php' => '8.1.0-8.2.99',
+			'php' => '8.1.0-8.3.99',
 			'typo3' => '12.0.0-12.4.99',
 		],
 		'conflicts' => [],
