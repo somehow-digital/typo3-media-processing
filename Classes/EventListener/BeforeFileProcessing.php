42c04b8f2186202dbe3a0f5d588919e211335429
Thomas Deinhamer <code@thasmo.dev>
2024-09-01 19:32:25 +0200

    Determine and set dimensions of PDF files prior processing

--- Classes/EventListener/BeforeFileProcessing.php
+++ Classes/EventListener/BeforeFileProcessing.php
@@ -2,6 +2,7 @@
 
 namespace SomehowDigital\Typo3\MediaProcessing\EventListener;
 
+use Smalot\PdfParser\Parser;
 use SomehowDigital\Typo3\MediaProcessing\ImageService\ImageServiceInterface;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Http\ApplicationType;
@@ -14,33 +15,46 @@ class BeforeFileProcessing
 
 	private ?array $configuration;
 
+	private Parser $parser;
+
 	public function __construct(
 		?ImageServiceInterface $service,
 		?ExtensionConfiguration $configuration,
+		Parser $parser,
 	) {
 		$this->service = $service;
 		$this->configuration = $configuration?->get('media_processing');
+		$this->parser = $parser;
 	}
 
 	public function __invoke(BeforeFileProcessingEvent $event): void
 	{
 		$context = ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST']);
-
 		$file = $event->getProcessedFile();
 
 		if (
 			(
-				($context->isBackend() && $this->configuration['common']['backend']) ||
-				($context->isFrontend() && $this->configuration['common']['frontend'])
+				$context->isBackend() && $this->configuration['common']['backend'] ||
+				$context->isFrontend() && $this->configuration['common']['frontend']
 			) &&
-			(
-				$file->getTask()->getSourceFile()->exists() &&
-				$file->getTask()->getSourceFile()->getProperty('width') &&
-				$file->getTask()->getSourceFile()->getProperty('height')
-			) &&
+			$event->getFile()->exists() &&
 			$this->service?->hasConfiguration() &&
 			$this->service?->canProcessTask($file->getTask())
 		) {
+			if (
+				$event->getFile()->getMimeType() === 'application/pdf' &&
+				!$event->getFile()->getProperty('width') &&
+				!$event->getFile()->getProperty('height')
+			) {
+				$document = $this->parser->parseFile($event->getFile()->getForLocalProcessing());
+				$details = current($document->getPages())->getDetails();
+
+				$event->getFile()->getMetaData()->add([
+					'width' => (int) $details['MediaBox'][2] ?: 0,
+					'height' => (int) $details['MediaBox'][3] ?: 0,
+				]);
+			}
+
 			if ($this->needsReprocessing($file)) {
 				$file->delete();
 			} else {
