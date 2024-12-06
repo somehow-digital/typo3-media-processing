2d2ab9d9841043e833b59a5b097d809895fa8fa6
Thomas Deinhamer <code@thasmo.dev>
2024-12-06 16:12:04 +0100

    Update extension configuration to support TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,8 +16,8 @@ $EM_CONF[$_EXTKEY] = [
 	],
 	'constraints' => [
 		'depends' => [
-			'php' => '8.1.0-8.3.99',
-			'typo3' => '12.0.0-12.4.99',
+			'php' => '8.1.0-8.4.99',
+			'typo3' => '12.0.0-13.4.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
