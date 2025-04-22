879753e536002bc139a2c5b84c07908c81574e59
Thomas Deinhamer <code@thasmo.dev>
2025-04-22 16:11:22 +0200

    Update URI source class

--- Classes/UriBuilder/ImglabUriSource.php
+++ Classes/UriBuilder/ImglabUriSource.php
@@ -4,19 +4,22 @@ declare(strict_types=1);
 
 namespace SomehowDigital\Typo3\MediaProcessing\UriBuilder;
 
+use SomehowDigital\Typo3\MediaProcessing\Utility\OnlineMediaUtility;
 use TYPO3\CMS\Core\Resource\FileInterface;
 
 class ImglabUriSource implements UriSourceInterface
 {
 	public function getSource(FileInterface $file): string
 	{
-		return $this->build($file);
+		$url = OnlineMediaUtility::getPreviewImage($file) ?? $file->getPublicUrl();
+
+		return $this->build($url);
 	}
 
-	private function build(FileInterface $source): string
+	private function build(string $url): string
 	{
-		$path = parse_url($source->getPublicUrl(), PHP_URL_PATH);
-		$query = parse_url($source->getPublicUrl(), PHP_URL_QUERY) ?? '';
+		$path = parse_url($url, PHP_URL_PATH);
+		$query = parse_url($url, PHP_URL_QUERY) ?? '';
 
 		return implode('?', array_filter([trim($path, '/'), trim($query)]));
 	}
