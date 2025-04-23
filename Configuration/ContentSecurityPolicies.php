23f5166c33b7c5af30a3552967c22f3eef1276e4
Thomas Deinhamer <code@thasmo.dev>
2025-04-23 11:13:31 +0200

    Fix CSP configuration

--- Configuration/ContentSecurityPolicies.php
+++ Configuration/ContentSecurityPolicies.php
@@ -8,7 +8,6 @@ use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Mutation;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationCollection;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationMode;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope;
-use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceScheme;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
 use TYPO3\CMS\Core\Type\Map;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -17,12 +16,14 @@ try {
 	$service = GeneralUtility::makeInstance(ImageServiceFactory::class)();
 
 	$collection = $service?->hasConfiguration() ? [
-		new Mutation(MutationMode::Extend, Directive::ImgSrc, SourceScheme::http, new UriValue($service?->getEndpoint())),
-		new Mutation(MutationMode::Extend, Directive::ImgSrc, SourceScheme::https, new UriValue($service?->getEndpoint())),
+		new Mutation(MutationMode::Extend, Directive::ImgSrc, new UriValue($service?->getEndpoint())),
 	] : [];
 
 	return Map::fromEntries([
 		Scope::backend(),
+		new MutationCollection(...$collection),
+	], [
+		Scope::frontend(),
 		new MutationCollection(...$collection),
 	]);
 } catch (Throwable) {
